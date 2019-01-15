b1b2ef1d38246a65d325ab4a13997e640c4ee888
Henri Hamunen <henri.hamunen@gmail.com>
2019-01-15 17:25:20 +0000

    Some notes for lectures 3-5

---
+++ lecture-notes/3-5. IIFEs, scopes, closures.js
@@ -0,0 +1,9 @@
+/*
+IIFE = Immediately Invoked Function Expression
+Used to limit scope to a library.
+
+Closure = scope of the surrounding function
+Local = current function scope
+Global = window object
+
+*/
